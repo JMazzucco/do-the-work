d31f1aee5c20226e2a9dfb5b27a012471f9282a7
John Mazzucco <John.Mazzucco@gmail.com>
2016-06-27 19:55:50 -0400

    Include and inject theAngular UI-Router

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui-router']);
 
 
 app.factory('posts', [ function(){
 	var o = {
-		posts: []
+		posts: [
+	  {title: 'post 1', upvotes: 5},
+	  {title: 'post 2', upvotes: 2},
+	  {title: 'post 3', upvotes: 15},
+	  {title: 'post 4', upvotes: 9},
+	  {title: 'post 5', upvotes: 4}
+	];
 	};
 return o;
 
@@ -14,13 +20,7 @@ app.controller('MainCtrl', [
 '$scope',
 'posts',
 function($scope, posts){
-  $scope.posts = [
-	  {title: 'post 1', upvotes: 5},
-	  {title: 'post 2', upvotes: 2},
-	  {title: 'post 3', upvotes: 15},
-	  {title: 'post 4', upvotes: 9},
-	  {title: 'post 5', upvotes: 4}
-	];
+  $scope.posts = posts.posts;
 
 	$scope.addPost = function() {
 
