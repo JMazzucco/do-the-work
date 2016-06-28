97719b63f66d78cb7b0f8f7b8f31610e2483e0d2
John Mazzucco <John.Mazzucco@gmail.com>
2016-06-28 07:39:19 -0400

    Add a function that adds a new post comment to an array

--- app.js
+++ app.js
@@ -79,4 +79,14 @@ function($scope, $stateParams, posts){
 
 	$scope.post = posts.post[$stateParams.id];
 
+	$scope.addComment = function(){
+		if($scope.body === '') {return;}
+		$scope.post.comments.push({
+			body: $scope.body,
+			author: 'user',
+			upvotes: 0
+		});
+		$scope.body = '';
+	};
+
 }]);
