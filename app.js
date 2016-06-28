01e93ffcf33097d99fc3e4e29ed073abdd7cb610
John Mazzucco <John.Mazzucco@gmail.com>
2016-06-27 20:49:23 -0400

    Create an individual page for each post

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-var app = angular.module('flapperNews', ['ui-router']);
+var app = angular.module('flapperNews', ['ui.router']);
 
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl'
+		})
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
+		})
+
+	$urlRouterProvider.otherwise('home');
+
+}]);
 
 app.factory('posts', [ function(){
+
 	var o = {
 		posts: [
-	  {title: 'post 1', upvotes: 5},
-	  {title: 'post 2', upvotes: 2},
-	  {title: 'post 3', upvotes: 15},
-	  {title: 'post 4', upvotes: 9},
-	  {title: 'post 5', upvotes: 4}
-	];
+		  {title: 'post 1', upvotes: 5},
+		  {title: 'post 2', upvotes: 2},
+		  {title: 'post 3', upvotes: 15},
+		  {title: 'post 4', upvotes: 9},
+		  {title: 'post 5', upvotes: 4}
+		]
 	};
+
 return o;
 
 }]);
@@ -27,6 +46,16 @@ function($scope, posts){
 		if(!$scope.title || $scope.title === '') {return;}
 
 		$scope.posts.push({
+		  title: $scope.title,
+		  link: $scope.link,
+		  upvotes: 0,
+		  comments: [
+		    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+		    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+		  ]
+		});
+
+		$scope.posts.push({
 			title: $scope.title,
 			link: $scope.link,
 			upvotes: 0
@@ -40,4 +69,14 @@ function($scope, posts){
 	$scope.incrementUpvotes = function(post) {
 		post.upvotes += 1;
 	}
+}]);
+
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+
+	$scope.post = posts.post[$stateParams.id];
+
 }]);
