c77e651e26a310ce0df21ca91c9892625835c944
John Mazzucco <John.Mazzucco@gmail.com>
2016-07-01 15:08:27 -0400

    Fix bug

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -4,9 +4,14 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
 
 	$stateProvider
 		.state('home', {
-			url: '/home',
-			templateUrl: '/home.html',
-			controller: 'MainCtrl'
+		  url: '/home',
+		  templateUrl: '/home.html',
+		  controller: 'MainCtrl',
+		  resolve: {
+		    postPromise: ['posts', function(posts){
+		      return posts.getAll();
+		    }]
+		  }
 		})
 		.state('posts', {
 			url: '/posts/{id}',
@@ -18,7 +23,9 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
 
 }]);
 
-app.factory('posts', ['http', function($http){
+app.factory('posts', ['$http', function($http){
+
+
 
 	var o = {
 		posts: [
@@ -30,15 +37,13 @@ app.factory('posts', ['http', function($http){
 		]
 	};
 
+  o.getAll = function() {
+    return $http.get('/posts').success(function(data){
+      angular.copy(data, o.posts);
+    });
+  };
+
 return o;
-
-	o.getAll = function() {
-		return $http.get('/posts').success(function(data){
-			angular.copy(data, o.posts);
-		})
-
-	});
-};
 
 }]);
 
