f585c3a06a57bcbf6df487c808f35e9532b27fec
John Mazzucco <John.Mazzucco@gmail.com>
2016-07-04 14:11:17 -0400

    Increment upvotes of comments

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -120,17 +120,19 @@ function($scope, posts, post){
 
 	$scope.post = post;
 
+	$scope.addComment = function(){
+	  if($scope.body === '') { return; }
+	  posts.addComment(post._id, {
+	    body: $scope.body,
+	    author: 'user',
+	  }).success(function(comment) {
+	  	$scope.post.comments.push(comment);
+	  });
+	  $scope.body = '';
+	};
 
-
-$scope.addComment = function(){
-  if($scope.body === '') { return; }
-  posts.addComment(post._id, {
-    body: $scope.body,
-    author: 'user',
-  }).success(function(comment) {
-  	$scope.post.comments.push(comment);
-  });
-  $scope.body = '';
-};
+	$scope.incrementUpvotes = function(comment) {
+		posts.upvoteComment(post, comment);
+	};
 
 }]);
