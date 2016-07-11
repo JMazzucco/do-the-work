fc02b753042f16da5e419725d3fd6dad26b67e56
John Mazzucco <John.Mazzucco@gmail.com>
2016-07-11 16:55:56 -0400

    Create a login function that posts a user to our /login route and saves the token returned.

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -67,6 +67,11 @@ app.factory('auth', ['$http', '$window', function($http, $window){
 		});
 	};
 
+	auth.logIn = function(user){
+		return $http.post('/login', user).success(function(data){
+			auth.saveToken(data.token);
+		});
+	};
 
 	return auth;
 
